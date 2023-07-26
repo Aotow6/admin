c296c8919a434bde5a26fc298ab683ce28d0acee
Aotow6 <satria.rajawali3@gmail.com>
2023-07-26 15:04:48 +0800

    register nya ditambahi

--- register.php
+++ register.php
@@ -26,22 +26,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
         $error_message = "Password harus terdiri dari minimal 5 karakter.";
     } else {
         // Mengamankan query dengan prepared statement
-        $query = "INSERT INTO users (username, email, password, is_admin) VALUES (?, ?, ?, ?)";
-        $stmt = mysqli_prepare($connect, $query);
+        $query_user = "INSERT INTO users (username, email, password, is_admin) VALUES (?, ?, ?, ?)";
+        $stmt_user = mysqli_prepare($connect, $query_user);
         $is_admin = 0; // Nilai default untuk is_admin (tidak admin)
-        mysqli_stmt_bind_param($stmt, "sssi", $username, $email, $password, $is_admin);
-        if (mysqli_stmt_execute($stmt)) {
-            // Jika registrasi berhasil, simpan pesan sukses di session
-            $_SESSION['success_message'] = "Registrasi berhasil. Silakan login dengan akun Anda.";
-            // Redirect ke halaman login
-            header("Location: login.php");
-            exit();
+        mysqli_stmt_bind_param($stmt_user, "sssi", $username, $email, $password, $is_admin);
+        
+        if (mysqli_stmt_execute($stmt_user)) {
+            // Jika registrasi ke tabel users berhasil, dapatkan nilai user_id yang baru saja di-generate
+            $user_id = mysqli_insert_id($connect);
+            
+            // Selanjutnya simpan data guru ke dalam tabel data_guru
+            $nama_lengkap = $_POST['nama_lengkap'];
+            $NIP = $_POST['NIP'];
+            $NUPTK = $_POST['NUPTK'];
+            $gelar = $_POST['gelar'];
+            $tanggal_lahir = $_POST['tanggal_lahir'];
+            $tempat_lahir = $_POST['tempat_lahir'];
+            $jenjang_ngajar = $_POST['jenjang_ngajar'];
+            $NPSN_sekolah = $_POST['NPSN_sekolah'];
+            $Nama_sekolah = $_POST['Nama_sekolah'];
+
+            $query_guru = "INSERT INTO data_guru (user_id, nama_lengkap, NIP, NUPTK, gelar, tanggal_lahir, tempat_lahir, jenjang_ngajar, NPSN_sekolah, Nama_sekolah) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $stmt_guru = mysqli_prepare($connect, $query_guru);
+            mysqli_stmt_bind_param($stmt_guru, "isssssssis", $user_id, $nama_lengkap, $NIP, $NUPTK, $gelar, $tanggal_lahir, $tempat_lahir, $jenjang_ngajar, $NPSN_sekolah, $Nama_sekolah);
+            
+            if (mysqli_stmt_execute($stmt_guru)) {
+                // Jika registrasi ke tabel ata_guru berhasil, simpan pesan sukses di session
+                $_SESSION['success_message'] = "Registrasi berhasil. Silakan login dengan akun Anda.";
+                // Redirect ke halaman login
+                header("Location: login.php");
+                exit();
+            } else {
+                $error_message = "Registrasi gagal. Silakan coba lagi.";
+            }
+
+            // Menutup statement guru
+            mysqli_stmt_close($stmt_guru);
         } else {
             $error_message = "Registrasi gagal. Silakan coba lagi.";
         }
 
+        // Menutup statement user
+        mysqli_stmt_close($stmt_user);
         // Menutup koneksi database
-        mysqli_stmt_close($stmt);
         mysqli_close($connect);
     }
 
@@ -49,8 +76,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
     $username_value = $username;
     $email_value = $email;
 }
-
-
 ?>
 
 
@@ -124,7 +149,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
 </head>
 <body>
     <div class="container">
-        <h2>Sistem Registrasi</h2>
+        <h2>Sistem Registrasi Guru</h2>
         <?php if (isset($error_message)) { ?>
             <p class="error-message"><?php echo $error_message; ?></p>
         <?php } ?>
@@ -143,6 +168,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
                 <label for="password">Password:</label>
                 <input type="password" id="password" name="password"  required>
             </div>
+
+            <!-- Input untuk data guru -->
+            <div class="form-group">
+                <label for="nama_lengkap">Nama Lengkap Guru:</label>
+                <input type="text" id="nama_lengkap" name="nama_lengkap" required>
+            </div>
+
+            <div class="form-group">
+                <label for="NIP">NIP:</label>
+                <input type="text" id="NIP" name="NIP" required>
+            </div>
+
+            <div class="form-group">
+                <label for="NUPTK">NUPTK:</label>
+                <input type="text" id="NUPTK" name="NUPTK" required>
+            </div>
+
+            <div class="form-group">
+                <label for="gelar">Gelar:</label>
+                <input type="text" id="gelar" name="gelar" required>
+            </div>
+
+            <div class="form-group">
+                <label for="tanggal_lahir">Tanggal Lahir:</label>
+                <input type="date" id="tanggal_lahir" name="tanggal_lahir" required>
+            </div>
+
+            <div class="form-group">
+                <label for="tempat_lahir">Tempat Lahir:</label>
+                <input type="text" id="tempat_lahir" name="tempat_lahir" required>
+            </div>
+
+            <div class="form-group">
+                <label for="jenjang_ngajar">Jenjang Mengajar:</label>
+                <input type="text" id="jenjang_ngajar" name="jenjang_ngajar" required>
+            </div>
+
+            <div class="form-group">
+                <label for="NPSN_sekolah">NPSN Sekolah:</label>
+                <input type="text" id="NPSN_sekolah" name="NPSN_sekolah" required>
+            </div>
+
+            <div class="form-group">
+                <label for="Nama_sekolah">Nama Sekolah:</label>
+                <input type="text" id="Nama_sekolah" name="Nama_sekolah" required>
+            </div>
+            <!-- End input untuk data guru -->
 
             <div class="form-group">
                 <input type="submit" name="submit" value="Register">
